40e00d615aa25a68839e56e0b476a3404dc08451
Armen Haddad <67371378+Armen-GH@users.noreply.github.com>
2021-05-09 17:42:12 +0200

    Update homework.cpp

--- Week 13/homework.cpp
+++ Week 13/homework.cpp
@@ -1,11 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <fstream>
-#include <set>
-#include <list>
-#include <iterator>
-using namespace std;
+#include "std_lib_facilities.h"
 
 struct Item //1
 {
@@ -43,28 +36,32 @@ struct Cmp_by_value
     bool operator()(const Item &a, const Item &b) const{return a.value > b.value;}
 };
 
-void nameDelete(string Name, vector<Item>& v)
-{
-    for (int i=0; i!=v.size(); i++)
-    {
-      if (v[i].name == Name)
-      {
-          v.erase(v.begin()+i,v.begin()+1+i);
-          break;
-      }    
-    }
-}
+class nameDelete {
+    string name;
+public:
+    nameDelete(const string& nn) : name(nn) { }
+    bool operator()(const Item& x) const { return x.name == name; }
+};
 
-void idDelete(int n, vector<Item>& v)
+class idDelete {
+    int iid;
+public:
+    idDelete(int id) : iid(id) { }
+    bool operator()(const Item& x) const { return x.iid == iid; }
+};
+
+class to_insert {
+    double val;
+public:
+    to_insert(double vv) : val(vv) { }
+    bool operator()(const Item& x) const { return x.value < val; }
+};
+
+template<typename C> void printout(const C& c, char sep = ' ')
 {
-    for (int i=0; i!=v.size(); i++)
-    {
-      if (v[i].iid == n)
-      {
-          v.erase(v.begin()+i,v.begin()+1+i);
-          break;
-      }    
-    }
+    for (const auto& a : c)
+        cout << a << sep;
+    cout << '\n';
 }
 
 int main()
@@ -79,70 +76,82 @@ int main()
     }
 
     cout << "Vector at the beginning:   ";
-    for(i=0;i<vi.size();i++)
-    {
-        cout << vi[i] << " ";
-    }
+    printout(vi);
     cout << "\n \n";
 
     sort(vi.begin(), vi.end(), Cmp_by_name()); //3
-    cout << "Sorting by name:   ";
-    for(i=0;i<vi.size();i++)
-    {
-        cout << vi[i] << " ";
-    }
+    cout << "Vector Sorted by name:   ";
+    printout(vi);
     cout << "\n \n";
 
     sort(vi.begin(), vi.end(), Cmp_by_iid()); //4
-    cout << "Sorting by ID:   ";
-    for(i=0;i<vi.size();i++)
-    {
-        cout << vi[i] << " ";
-    }
+    cout << "Vector Sorted by ID:   ";
+    printout(vi);
     cout << "\n \n";
 
     sort(vi.begin(), vi.end(), Cmp_by_value()); //5
-    cout << "Sorting by value:   ";
-    for(i=0;i<vi.size();i++)
-    {
-        cout << vi[i] << " ";
-    }
+    cout << "Vector Sorted by value:   ";
+    printout(vi);
     cout << "\n \n";
 
     Item insert1 = {"horse shoe",99,12.34}; //6
     Item insert2 = {"Canon S400", 9988,499.95};
     vi.push_back(insert1);
     vi.push_back(insert2);
-    cout << "After two insertions:   ";
-    for(i=0;i<vi.size();i++)
-    {
-        cout << vi[i] << " ";
-    }
+    cout << "Vector after two insertions:   ";
+    printout(vi);
     cout << "\n \n";
 
-    nameDelete("car", vi); // 7
-    nameDelete("can", vi);
-    cout << "After two name-based deletions:   ";
-    for(i=0;i<vi.size();i++)
-    {
-        cout << vi[i] << " ";
-    }
+    vi.erase(find_if(vi.begin(), vi.end(), nameDelete("bye"))); // 7
+    vi.erase(find_if(vi.begin(), vi.end(), nameDelete("grade")));
+    cout << "Vector after two name-based deletions:   ";
+    printout(vi);
     cout << "\n \n";
 
-    idDelete(2, vi); //8
-    idDelete(89, vi);
-    cout << "After two id-based deletions:   ";
-    for(i=0;i<vi.size();i++)
-    {
-        cout << vi[i] << " ";
-    }
+    vi.erase(find_if(vi.begin(), vi.end(), idDelete(187))); //8
+    vi.erase(find_if(vi.begin(), vi.end(), idDelete(22)));
+    cout << "Vector after two id-based deletions:   ";
+    printout(vi);
     cout << "\n \n";
 
-    //list instead of a vector
-    list<Item> li;
-    
-    cout << "\n \n \n";
+    //9 list instead of a vector
+    list<Item> li (vi.size());
+    copy(vi.begin(), vi.end(), li.begin());
     cout << "List at the beginning:   ";
+    printout(li);
+    cout << "\n \n";
     
-}
+    li.sort(Cmp_by_name());
+    cout << "List sorted by name:   ";
+    printout(li);
+    cout << "\n \n";
+    
+    li.sort(Cmp_by_iid());
+    cout << "List sorted by ID:   ";
+    printout(li);
+    cout << "\n \n";
 
+    li.sort(Cmp_by_value());
+    cout << "List sorted by value:   ";
+    printout(li);
+    cout << "\n \n";
+
+    Item insert3 = {"Prom",9123,12.344};
+    Item insert4 = {"code",812,144.34};
+    li.insert(find_if(li.begin(), li.end(), to_insert(insert3.value)), insert3);
+    li.insert(find_if(li.begin(), li.end(), to_insert(insert4.value)), insert4);
+    cout << "List after two insertions:   ";
+    printout(li);
+    cout << "\n \n";
+
+    li.erase(find_if(li.begin(), li.end(), nameDelete("hello")));
+    li.erase(find_if(li.begin(), li.end(), nameDelete("eye")));
+    cout << "List after two name-based deletions: ";
+    printout(li);
+    cout << "\n \n";
+
+    li.erase(find_if(li.begin(), li.end(), idDelete(6)));
+    li.erase(find_if(li.begin(), li.end(), idDelete(10)));
+    cout << "List after two id-based deletions: ";
+    printout(li);
+}
