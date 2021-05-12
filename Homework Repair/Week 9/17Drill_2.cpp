6477a40226cef5438d079acbcefea7927b8ab53d
Armen Haddad <67371378+Armen-GH@users.noreply.github.com>
2021-05-12 18:53:18 +0200

    Update 17Drill_2.cpp

--- Homework Repair/Week 9/17Drill_2.cpp
+++ Homework Repair/Week 9/17Drill_2.cpp
@@ -1,15 +1,14 @@
-#include "std_lib_facilities.h"
+#include "std_lib_facilities"
 
 void print_array(ostream& os, int* a, int n);
 
 int main()
 {
-    int e;
     //1
-    int a = 7;
+    int a = 7, e;
     int *p1 = &a;
     //2
-    cout << "p1 = " << p1 << ", it points to = " << *p1;
+    cout << "p1 = " << p1 << ", it points to = " << *p1 << endl;
     //3
     int *arr = new int[7];
     int *p2 = arr;
@@ -18,33 +17,31 @@ int main()
         arr[i] = (i + 1) * 2;
     }
     //4
-    cout << p2 << endl;
+    cout << "Value of P2 is " << p2 << endl;
     print_array(cout, arr, 7);
     //5 6 7
     int *p3 = p2;
     p2 = p1;
-    p1 = p3;
+    p2 = p3;
     //8
-    cout << "p1 = " << p1 << ", it points to = " << *p1;
-    cout << "p2 = " << p1 << ", it points to = " << *p2;
+    cout << "p1 = " << p1 << ", it points to = " << *p1 << endl;
+    cout << "p2 = " << p2 << ", it points to = " << *p2 << endl;
     //9
-    delete[] p2;
+    delete[] arr;
     //10
     int *arr1 = new int[10];
-    int *p1 = arr1;
+    p1 = arr1;
     for(int i=0;i<10;i++)
     {
         arr1[i] = (i + 1) * 2;
     }
-    int *p1 = arr1;
     //11
     int *arr2 = new int[10];
-    int *p2 = arr2;
+    p2 = arr2;
     for(int i=0;i<10;i++)
     {
-        p1[i] = p2[i]; 
+        p2[i] = p1[i]; 
     }
-    int *p1 = arr1;
     delete[] arr1;
     delete[] arr2;
     //12
@@ -55,10 +52,10 @@ int main()
         v[i] = (i + 1) * 2;
     }
     vector<int> v1;
-    vector<int>* vp1 = &v;
+    vector<int>* vp1 = &v1;
     for(int i=0;i<10;i++)
     {
-        vp[i] = vp1[i];
+        vp1[i] = vp[i];
     }
 }
 
