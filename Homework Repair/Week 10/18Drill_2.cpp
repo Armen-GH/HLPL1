84b223d1deedbb6ab92f7a0011142393e3d33a8d
Armen Haddad <67371378+Armen-GH@users.noreply.github.com>
2021-05-13 00:15:24 +0200

    Update 18Drill_2.cpp

--- Homework Repair/Week 10/18Drill_2.cpp
+++ Homework Repair/Week 10/18Drill_2.cpp
@@ -1,5 +1,5 @@
 #include "std_lib_facilities.h"
-//1
+
 vector<int> gv = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
 //2
 void f(vector<int> v);
@@ -11,7 +11,7 @@ int main()
     vector<int> vv;
     for(int i=0;i<10;i++) //4.b
     {
-        vv[i] = factorial(i+1);
+        vv.push_back(factorial(i+1));
     }
     f(vv); //4.c
 }
@@ -19,25 +19,28 @@ int main()
 void f(vector<int> v)
 {
     vector<int> lv; //3.a
+    lv.reserve(v.size());
     for(int i=0;i<10;i++) //3.b
     {
-        lv[i] = gv[i];
+        lv.push_back(gv[i]);
     }
     for(int i=0;i<10;i++) //3.c
     {
         cout << lv[i] << " ";
     }
+    cout << "\n" << endl;
     vector<int> lv2 = v; //3.d
     for(int i=0;i<lv2.size();i++) //3.e
     {
         cout << lv2[i] << " ";
     }
+    cout << "\n" << endl;
 }
 
 int factorial(int a)
 {
-    int i=1, f=1;
-    for(i; i<=a; i++)
+    int i, f=1;
+    for(i=1; i<=a; i++)
     {
         f = f * i;
     }
